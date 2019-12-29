a79b86a301c659c80aed684cc194f7d0948e9769
Lee Bardon <leerbardon@gmail.com>
2019-12-29 15:47:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each do |i|
-    print "#{i}"
+def roll_call_dwarves(array)
+  array.each_with_index do |item, index|
+    puts "#{index + 1}. #{item}"
   end
 end
 
