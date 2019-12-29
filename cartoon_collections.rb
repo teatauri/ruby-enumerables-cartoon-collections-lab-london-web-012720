481aec2fbf4452e5054caaa0a2c6f276df2f7518
Lee Bardon <leerbardon@gmail.com>
2019-12-29 15:47:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summons_captain_planet(array)
+  array.map {|memo| memo.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
